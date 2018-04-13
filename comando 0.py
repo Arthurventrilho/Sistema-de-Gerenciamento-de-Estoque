255434bc54e9f4fe837dcb5157fb9736b89779f0
gianlucca napolitano <gian.napolitano24@gmail.com>
2018-04-13 09:19:41 -0300

    fiz o comando 0

---
+++ comando 0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 13 09:17:47 2018
+
+@author: gian.napolitano
+"""
+
+#Sair e parar o ciclo
+    if Comando == 0:   
+        print('Até Mais!')
+        break
